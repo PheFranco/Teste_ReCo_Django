a7ac82bf55f87c09c63ac1d70f238132e4cb4e5d
giuliasantiagoo178 <giulia54658446@edu.df.senac.br>
2025-11-25 19:43:07 +0000

    chore(marketplace): add template and register app in settings

--- Recco/ReCo/settings.py
+++ Recco/ReCo/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuario',  # <-- manter/adicionar
+    'marketplace',  # app do marketplace
 ]
             
 
